396c6525a3e3600edbd10844709729cd4afe6b91
Jesse <jessesilber@gmail.com>
2016-11-21 15:43:55 -0500

    added list view skeleton code for the restaurant adapter created Restaurant object TODO: implement adapter for listview ----- implement fragments java code/clean up the fragment code ----- be sure to allow the list fragment to extend the listfragment, look at her online code for the examples and a cleaner copy.

--- app/src/main/java/com/nough/gouda/goudanough/Restaurant.java
+++ app/src/main/java/com/nough/gouda/goudanough/Restaurant.java
@@ -1,8 +1,81 @@
 package com.nough.gouda.goudanough;
 
+
 /**
  * Created by 1133611 on 11/21/2016.
  */
 public class Restaurant {
-    private 
+    private String name;
+    private String url;
+    private String cuisine;
+    private int price_range;
+    private double latitude;
+    private double longitude;
+    private String featured_image;
+
+    public Restaurant(String name, String url, String cuisine, int price_range, double latitude, double longitude, String img_src){
+        this.name = name;
+        this.url = url;
+        this.cuisine = cuisine;
+        this.price_range = price_range;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.featured_image = img_src;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public int getPrice_range() {
+        return price_range;
+    }
+
+    public void setPrice_range(int price_range) {
+        this.price_range = price_range;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getFeatured_image() {
+        return featured_image;
+    }
+
+    public void setFeatured_image(String featured_image) {
+        this.featured_image = featured_image;
+    }
 }
