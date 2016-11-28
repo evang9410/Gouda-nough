0375a5364e7853f5b5dcd0cfcc2c5c315b3a248f
Jonathan Bizier <j0nbiz@outlook.com>
2016-11-28 15:45:57 -0500

    more ui stuff

--- app/src/main/java/com/nough/gouda/goudanough/MainActivity.java
+++ app/src/main/java/com/nough/gouda/goudanough/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
