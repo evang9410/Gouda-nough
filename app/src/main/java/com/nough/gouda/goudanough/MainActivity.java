4974d81b74f197ab9905a34774c584b3f92533fe
Jesse <jessesilber@gmail.com>
2016-11-29 11:28:40 -0500

    menu is being implemented

--- app/src/main/java/com/nough/gouda/goudanough/MainActivity.java
+++ app/src/main/java/com/nough/gouda/goudanough/MainActivity.java
@@ -5,10 +5,13 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -70,4 +73,26 @@ public class MainActivity extends AppCompatActivity implements Navigation.OnNavi
         adapter.setDataset(favourite_restaurants);
         list_title.setText("Favourites");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //about page
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
