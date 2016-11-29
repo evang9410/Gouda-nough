70449239fd47879de91c13a6bbc6dde1e5f70a19
Ryan Sena <railanderson@gmail.com>
2016-11-29 11:35:08 -0500

    are all fields entered done

--- app/src/main/java/com/nough/gouda/goudanough/activities/AddRestoActivity.java
+++ app/src/main/java/com/nough/gouda/goudanough/activities/AddRestoActivity.java
@@ -2,6 +2,7 @@ package com.nough.gouda.goudanough.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,13 +27,43 @@ public class AddRestoActivity extends Activity {
     private Button cancel_button;//prolly not needed.
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_resto);
         initFields();
         setSpinners();
         dao = DBHelper.getDBHelper(this);
 
+    }
+
+    public void onSaveClicked(View v){
+        if(areAllFieldsEntered())
+        {
+            //// TODO: 11/29/2016 save to the database
+        }
+        else{
+            // TODO: 11/29/2016 display toast
+        }
+    }
+
+    public void onCancelClicked(View v){
+
+    }
+
+    private boolean areAllFieldsEntered(){
+        if(resto_name == null || cuisine == null || phone_number == null)
+            return false;
+
+        String name = resto_name.getText().toString();
+        String cuisine = resto_name.getText().toString();
+        String phone = phone_number.getText().toString();
+        String ratingSelected = rating_spinner.getSelectedItem().toString();
+        String priceSelected = price_spinner.getSelectedItem().toString();
+
+        if(name.isEmpty() || cuisine.isEmpty() ||
+                phone.isEmpty() || ratingSelected.isEmpty() || priceSelected.isEmpty())
+            return false;
+        return true;
     }
 
     /**
@@ -57,12 +88,17 @@ public class AddRestoActivity extends Activity {
         ArrayAdapter<CharSequence> ratingsOptions =
                 ArrayAdapter.createFromResource(this,R.array.rating_spin,android.R.layout.simple_spinner_item);
         ratingsOptions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rating_spinner.setPrompt("Rate the resto!");
         rating_spinner.setAdapter(ratingsOptions);
+
 
         //set price spinner
         ArrayAdapter<CharSequence> pricesOptions =
                 ArrayAdapter.createFromResource(this,R.array.price_range_spin,android.R.layout.simple_spinner_item);
         pricesOptions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        price_spinner.setPrompt("How expensive the food is ?");
         price_spinner.setAdapter(pricesOptions);
     }
+
+
 }
